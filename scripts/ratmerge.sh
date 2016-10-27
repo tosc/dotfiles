807d1b6448fa8ada9eb1277014d5ab929e4f251d
Opo <opo@OpoArch.lan>
2016-10-27 02:04:34 +0200

    Ratpoison: Better statusbar.

--- scripts/ratmerge.sh
+++ scripts/ratmerge.sh
@@ -3,10 +3,13 @@ if [ "$mergeWindow" == "" ]; then
 	echo "Don't merge"	
 else
 	echo "Merge"
-	oldW="$(ratpoison -c 'curframe')"
+
+	curFrame="$(~/git/dotfiles/scripts/ratframe.sh 3)"
+	oldW=$(echo $curFrame | cut -f3 -d-)
+	sortNr=$(echo $curFrame | cut -f5 -d-)
 	ratpoison -c "gselect f$mergeWindow"
 	ratpoison -c "gmerge f$oldW"
 	ratpoison -c "gdelete f$oldW"
 	ratpoison -c remove
-	rm ~/tmp/ratf/$oldW
+	rm ~/tmp/ratf/*
 fi
--- scripts/xmorat.sh
+++ scripts/xmorat.sh
@@ -1,12 +1,24 @@
-test=$(~/git/dotfiles/scripts/ratwins.sh)
+mkdir -p ~/tmp/ratf/
 curFrame="$(~/git/dotfiles/scripts/ratframe.sh 3)"
 frameNr=$(echo $curFrame | cut -f3 -d-)
+sortNr=$(echo $curFrame | cut -f5 -d-)
 screenNr=$(echo $curFrame | cut -f4 -d-)
+info=$(
+	ratpoison -c "windows %s%x%n-%t" | while read p; do
+		echo $p
+	done
+)
+echo $info > ~/tmp/ratf/$sortNr-$frameNr
+
 echo $(
 	first=1
 	lastScreenNr=""
 	ls ~/tmp/ratf/ | while read p; do
 		frame=$(cat ~/tmp/ratf/$p)
+		if [ $first == 1 ]; then
+			first=0
+			lastScreenNr="${frame:1:1}"
+		fi
 		if [ "${frame:1:1}" == $lastScreenNr ]; then
 			:
 		else
@@ -21,14 +33,10 @@ echo $(
 					if [ "${i:0:1}" == "*" ]; then
 						echo "<fc=#ee9a00>${i:2:10}</fc> "
 					else
-						echo "<fc=#2ECC71>${i:2:10}</fc> "
+						echo "<fc=#F08080>${i:2:10}</fc> "
 					fi
 				else
-					if [ "${i:1:1}" == "$screenNr" ]; then
-						echo "<fc=#F08080>${i:2:10}</fc> "
-					else
-						echo "<fc=#696665>${i:2:10}</fc> "
-					fi
+					echo "<fc=#696665>${i:2:10}</fc> "
 				fi
 			done
 		fi
