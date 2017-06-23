de16bcdf36b61548decdecf369e78d89b8cb8787
tosc <tosc@tosc.com>
2017-06-23 16:41:53 +0200

    Script for OCR with JISHO.

--- scripts/ratswap.sh
+++
@@ -1,5 +0,0 @@
-ratpoison -c "gmove :$(~/git/dotfiles/scripts/ratframe.sh $1 $2)"
-ratpoison -c "select -"
-ratpoison -c "fselect $(~/git/dotfiles/scripts/ratframe.sh $1 $2)"
-sleep .1
-ratpoison -c other
